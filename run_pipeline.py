a803569964e50794a1046d61aabd429c63989ebd
Rishil Boddula <rishilboddula@gmail.com>
2025-11-02 16:30:23 +0000

    Add Updated  run pipeline file

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,7 +2,7 @@ from pipelines.training_pipeline import training_pipeline
 from zenml.client import Client
 import os
 if __name__ =="__main__":
-    print(Client().active_stack.experiment_tracker.get_tracking_uri())
+   
     workspace=os.getenv('WORKSPACE',os.getcwd())
     data_path=os.path.join(workspace,'datasets','olist_customers_dataset.csv')
     training_pipeline(data_path=data_path)
